051bf8e7f972cbed7ebcbb63f327bfca6ec41130
Lahiru Amarasooriya <lahirudx@gmail.com>
2023-08-31 07:57:40 +0530

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Lahiru Amarasooriya on 2023-08-31.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
