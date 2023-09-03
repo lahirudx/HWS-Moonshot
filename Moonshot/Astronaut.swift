e4ab646ba3ec9be22414ec6fa98c17442ea4985d
Lahiru Amarasooriya <lahirudx@gmail.com>
2023-09-03 22:18:07 +0530

    Project Completed

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Lahiru Amarasooriya on 2023-09-02.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
